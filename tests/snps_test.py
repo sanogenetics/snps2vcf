6cd763c346f2ab81c76260961ba7f5fb666d0a69
Adam Faulconbridge <adam@sanogenetics.com>
2022-10-31 14:27:50 +0000

    add testing

---
+++ tests/snps_test.py
@@ -0,0 +1,15 @@
+
+from io import StringIO
+from snps2vcf import Converter
+
+class TestSnps:
+    def test_converter(self):
+        input = StringIO()
+        # TODO create input
+
+        output = StringIO()
+
+        Converter().convert(input, output)
+
+        # TODO check output is valid VCF
+        # TODO check output is expected VCF
